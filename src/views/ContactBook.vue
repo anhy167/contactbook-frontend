bd8dd7f76fe6658c316f3ac6c4f07fd3d4c53696
anhy167 <ttka1607@gmail.com>
2022-04-05 23:47:29 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -122,7 +122,7 @@ export default {
                     <i class="fas fa-address-card"></i> 
                 </h4>
                 <ContactCard :contact="activeContact" /> 
-                <a
+                <router-link
                     :to="{
                         name: 'contact.edit',
                         params: { id: activeContact.id },
@@ -130,7 +130,7 @@ export default {
                 >
                     <span class="mt-2 badge badge-warning">
                         <i class="fas fa-edit"></i> Hiệu chỉnh</span>
-                </a> 
+                </router-link> 
             </div>
         </div>
     </div>
